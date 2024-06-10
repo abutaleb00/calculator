31e82d409a440c38e2cf8b509604c671e2aadc6f
Abu Taleb <abutaleb142@gmail.com>
2024-06-10 23:44:12 +0600

    Login logout added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import PrivateRoutes from "./utils/privateRoute";
+import PublicRoutes from "./utils/publicRoute";
+import BasicCalculator from "./pages/basicCalculator";
+import PremiumCalculator from "./pages/premiumCalculator";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route element={<PrivateRoutes />}>
+          <Route element={<PremiumCalculator />} path="/home" />
+        </Route>
+        <Route element={<PublicRoutes />}>
+          <Route element={<BasicCalculator />} path="/" />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/premiumCalculator/index.tsx
@@ -0,0 +1,187 @@
+import React from "react";
+
+const PremiumCalculator = () => {
+  return (
+    <div>
+      <div className="min-w-screen min-h-screen bg-gray-100 flex items-center justify-center px-5 py-5">
+        <div
+          className="w-full mx-auto rounded-xl bg-gray-100 shadow-xl text-gray-800 relative overflow-hidden"
+          style={{ maxWidth: "500px" }}
+        >
+          <div className="w-full h-28 bg-[#f9dcb8]  flex items-end text-right">
+            <div className="w-full py-5 px-6 text-6xl text-black font-thin">
+              0
+            </div>
+          </div>
+          <div className="w-full bg-[#f9dcb8]">
+            <div className="flex w-full">
+              <div className="w-1/6 mx-[1px] my-[1px] ">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#f7ccb2] hover:bg-opacity-80 text-black text-xl font-light">
+                  MC
+                </button>
+              </div>
+              <div className="w-1/6 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#f7ccb2] hover:bg-opacity-80 text-black text-xl font-light">
+                  MR
+                </button>
+              </div>
+              <div className="w-1/6 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#f7ccb2] hover:bg-opacity-80 text-black text-xl font-light">
+                  M+
+                </button>
+              </div>
+              <div className="w-1/6 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#f7ccb2] hover:bg-opacity-80 text-black text-xl font-light">
+                  M-
+                </button>
+              </div>
+              <div className="w-1/6 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#f7ccb2] hover:bg-opacity-80 text-black text-xl font-light">
+                  MS
+                </button>
+              </div>
+              <div className="w-1/6 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#f7ccb2] hover:bg-opacity-80 text-black text-xl font-light">
+                  MV
+                </button>
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-xl font-light">
+                  C
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-xl font-light">
+                  +/-
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-xl font-light">
+                  %
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-2xl font-light">
+                  x
+                </button>
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-xl font-light">
+                  1/x
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-xl font-light">
+                  x²
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-xl font-light">
+                  ∛
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-2xl font-light">
+                  ÷
+                </button>
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  7
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  8
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  9
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5] hover:bg-opacity-80 text-black text-xl font-light">
+                  ⨉
+                </button>
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  4
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  5
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  6
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5]  hover:bg-opacity-80 text-black text-xl font-light">
+                  -
+                </button>
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  1
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  2
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  3
+                </button>
+              </div>
+              <div className="w-1/4  mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#ffe5d5]  hover:bg-opacity-80 text-black text-xl font-light">
+                  +
+                </button>
+              </div>
+            </div>
+            <div className="flex w-full">
+              <div className="w-1/4 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  +/-
+                </button>
+              </div>
+              <div className="w-1/4 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  0
+                </button>
+              </div>
+              <div className="w-1/4 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-white hover:bg-opacity-80 text-black text-xl font-light">
+                  .
+                </button>
+              </div>
+              <div className="w-1/4 mx-[1px] my-[1px]">
+                <button className="w-full h-16 outline-none focus:outline-none bg-[#b24c0c] hover:bg-opacity-80 text-white text-xl font-light">
+                  =
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default PremiumCalculator;
